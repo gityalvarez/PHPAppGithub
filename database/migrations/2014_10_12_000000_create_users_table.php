5fefc8ec889d346656e1f0816709da393ebdcb41
Leonardo <Leonardo@192.168.95.1>
2017-10-29 19:14:51 -0300

    Se actualiza con campos deleted_at donde faltaban...

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->decimal('longitud');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
