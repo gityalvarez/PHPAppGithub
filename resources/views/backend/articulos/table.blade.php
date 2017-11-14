6179cecd1a73d8ed63a34c809428425b621cf92e
gityalvarez <yamandu.alvarez@gmail.com>
2017-11-14 18:13:01 -0300

    Imagenes al articulo del backend
    Se agregan las imagenes del producto en el backend/articulos

--- resources/views/backend/articulos/table.blade.php
+++ resources/views/backend/articulos/table.blade.php
@@ -3,21 +3,28 @@
             <th>Stock</th>
             <th>Precio</th>
             <th>Producto</th>
+            <th>Imagen</th>
             <th>Comercio</th>
             <th colspan="3">Acciones</th>
         </tr>    
     @foreach($articulos as $articulo)
         <tr>
-            <td>{!! $articulo->stock !!}</td>
+            <td>{!! $articulo->stock !!}</td>   
             <td>{!! $articulo->precio !!}</td>
             <td>{!! $articulo->producto->nombre !!}</td>
+            @if (!empty($articulo->producto->imagen))
+                <td><img src="{{ asset('storage/'.$articulo->producto->imagen) }}" width="100" height="80"/></td>
+            @else
+                <td>Articulo sin imagen...</td>
+            @endif
             <td>{!! $articulo->comercio->nombre !!}</td>
             <td>
                 {!! Form::open(['route' => ['backend.articulos.destroy', $articulo->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
-                    <a href="{!! route('backend.articulos.show', [$articulo->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"> Ver</i></a>
-                    <a href="{!! route('backend.articulos.edit', [$articulo->id]) !!}" class='btn btn-default btn-xs'><i class="fa fa-pencil"> Editar</i></a>
-                    {!! Form::button('<i class="fa fa-trash"> Borrar</i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Realmente está seguro?')"]) !!}
+                    <a href="{!! route('backend.articulos.show', [$articulo->id]) !!}" class='btn btn-default'><i class="fa fa-eye"> Ver</i></a>
+            
+                    <a href="{!! route('backend.articulos.edit', [$articulo->id]) !!}" class='btn btn-default'><i class="fa fa-pencil"> Editar</i></a>
+                    {!! Form::button('<i class="fa fa-trash"> Borrar</i>', ['type' => 'submit', 'class' => 'btn btn-danger', 'onclick' => "return confirm('Realmente está seguro?')"]) !!}
                 </div>
                 {!! Form::close() !!}
             </td>
