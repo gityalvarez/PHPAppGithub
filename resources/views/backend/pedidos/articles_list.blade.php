ce646852bc27675fd400be7346bba235e9721875
gityalvarez <yamandu.alvarez@gmail.com>
2017-11-15 15:24:11 -0300

    articles_list con checkbox
    Se agrega la funcion de control con el checkbox
    Solo se muestra la cantidad de articulos si está seleccionado el articulo
    Falta arreglar: si se desselecciona el checkbox restar del total

--- resources/views/backend/pedidos/articles_list.blade.php
+++ resources/views/backend/pedidos/articles_list.blade.php
@@ -2,21 +2,27 @@
 
 <script type="text/javascript">
 
-$(document).on('change','.cantidad',function () {
-    var total = 0;
-
-    $('.item').each(function () {
-        
-        cantidad = parseInt($(this).find('.cantidad').val(),10) || 0;
-        precio = parseInt($(this).find('.precio').val(),10) || 0;
-        total += cantidad * precio;        
+$(document).ready(function(){
+    $('.checkbox').on('change',function(){
+        var $row = $(this).parents('tr');
+        $row.find('.cantidad').toggle('.cantidad');
+        $(document).on('change','.cantidad',function () {
+            var total = 0;
+            $('.item').each(function () {        
+                cantidad = parseInt($(this).find('.cantidad').val(),10) || 0;
+                precio = parseInt($(this).find('.precio').val(),10) || 0;
+                total += cantidad * precio;        
+            });
+            $('.total').html(total);
+        });
     });
-    $('.total').html(total);
 });
-
 </script>
 
+
 <h2>Total: <span class='total'></span></h2>
+
+
 
 
 <table class="table table-responsive table-striped" id="articles">    
@@ -33,22 +39,23 @@ $(document).on('change','.cantidad',function () {
         <tbody>
         <tr class='item'>
             <td>{!! $articulo->stock !!}</td>   
-            <td> <input name='precio' class='form-control precio' value="{!! $articulo->precio !!}"></td>
+            <td> <input name='precio' class='form-control precio' value="{!! $articulo->precio !!}" readonly='readonly'></td>
             <td>{!! $articulo->producto->nombre !!}</td>
             @if (!empty($articulo->producto->imagen))
                 <td><img src="{{ asset('storage/'.$articulo->producto->imagen) }}" width="100" height="80"/></td>
             @else
                 <td>Articulo sin imagen...</td>
             @endif
-            <td id='checkbox' class="checkbox"><label><input type="checkbox" value=""></label></div></td>
-            <td><input type="number" name='cantidad' class='form-control cantidad' min=0></td>
-            
+            <td><input type="checkbox" value="" class='checkbox'></td>
+            <td><input type="number" style="display:none"  name='cantidad' class='form-control cantidad' min=0 ></td>
         </tr>
         </tbody>
     @endforeach    
 </table>
 
-<div class="form-group col-sm-12">
+<div class="pull-right">
+<div class="form-group col-sm-12 ">
     {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
     <a href="{!! route('backend.pedidos.index') !!}" class="btn btn-default">Cancelar</a>
+</div>
 </div>
