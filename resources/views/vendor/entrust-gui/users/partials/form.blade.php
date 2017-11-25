88b971fce7c3ffe66461796426ca24bbd7f181ec
mmvs1985 <mmvs1985@gmail.com>
2017-11-25 03:09:38 -0300

    se cambia el form de usuarios nuevo/edit
    se agrego el geter de latiud y longitud automatico, ya basta con poner bien la calle y el numero de puerta y basta, carga los campos hidden y guarda los datos correctamente

--- resources/views/vendor/entrust-gui/users/partials/form.blade.php
+++ resources/views/vendor/entrust-gui/users/partials/form.blade.php
@@ -24,16 +24,24 @@
 </div>
 @endif
 <div class="form-group">
+    <label for="calle">Calle o Avenida</label>
+    <input type="name" class="form-control" id="calle" placeholder="nombre de la calle" name="calle" value="" onchange="geolocalizar()">
+</div>
+<div class="form-group">
+    <label for="puerta">Num de Puerta</label>
+    <input type="name" class="form-control" id="nopuerta" placeholder="puerta" name="puerta" value="" onchange="geolocalizar()" >
+</div>
+<div class="form-group">
     <label for="direccion">Direccion</label>
-    <input type="name" class="form-control" id="direccion" placeholder="Direccion" name="direccion" value="{{ (Session::has('errors')) ? old('direccion', '') : $user->direccion }}">
+    <input type="name" class="form-control" id="direccion" placeholder="Direccion" name="direccion" value="{{ (Session::has('errors')) ? old('direccion', '') : $user->direccion }}" hidden="true">
 </div>
 <div class="form-group">
     <label for="latitud">Latitud</label>
-    <input type="name" class="form-control" id="latitud" placeholder="Latitud" name="latitud" value="{{ (Session::has('errors')) ? old('latitud', '') : $user->latitud }}">
+    <input type="name" class="form-control" id="latitud" placeholder="Latitud" name="latitud" value="{{ (Session::has('errors')) ? old('latitud', '') : $user->latitud }}" hidden="true">
 </div>
 <div class="form-group">
     <label for="longitud">Longitud</label>
-    <input type="name" class="form-control" id="longitud" placeholder="Longitud" name="longitud" value="{{ (Session::has('errors')) ? old('longitud', '') : $user->longitud }}">
+    <input type="name" class="form-control" id="longitud" placeholder="Longitud" name="longitud" value="{{ (Session::has('errors')) ? old('longitud', '') : $user->longitud }}" hidden="true">
 </div>
 <div class="form-group">
     <label for="roles">Roles</label>
@@ -43,3 +51,28 @@
         @endforeach
     </select>
 </div>
+
+<script type="text/javascript">
+    
+    function geolocalizar() {
+
+    console.log( document.getElementById("direccion").value);
+    var dircomp = document.getElementById("nopuerta").value + ", "+ document.getElementById("calle").value + ", montevideo, uruguay";
+    inp =encodeURI(dircomp);
+    console.log(inp);
+    $.getJSON('http://nominatim.openstreetmap.org/search?format=json&limit=1&q=' + inp, function(data) {
+        var items = {lati:'',long:''};
+
+        $.each(data, function(key, val) {
+            items['lati'] = val.lat;
+            items['long']= val.lon;
+        });
+     document.getElementById("direccion").value = dircomp;
+     document.getElementById("latitud").value = items['lati'];
+     document.getElementById("longitud").value = items['long'];
+
+     });
+
+}
+
+</script>
